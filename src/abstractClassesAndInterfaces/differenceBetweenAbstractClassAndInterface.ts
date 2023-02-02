85121e6c4c3c3873e0c7fcdedf4a5a0eec5039b1
sychova <anastasiya.sychova@gmail.com>
2023-02-02 19:40:19 +0300

    section 6 part 11 difference between abstract and interface

--- src/abstractClassesAndInterfaces/differenceBetweenAbstractClassAndInterface.ts
+++ src/abstractClassesAndInterfaces/differenceBetweenAbstractClassAndInterface.ts
@@ -1,39 +1,60 @@
-abstract class Person {
-  public abstract name: string;
-  public abstract email: string;
-  public abstract phone: number;
+// abstract class Person {
+//   public abstract name: string;
+//   public abstract email: string;
+//   public abstract phone: number;
   
-  public greeting() {
-    console.log(`Hello ${this.name}`);
-  }
-}
+//   public greeting() {
+//     console.log(`Hello ${this.name}`);
+//   }
 
-interface User {
-  name: string;
-  email: string;
-  phone: string;
-}
+//   public static nameClass() {
+//     return "Class name is Person";
+//   }
+// }
 
-/////////
+// interface User {
+//   name: string;
+//   email: string;
+//   phone: number;
+//   greeting: () => void;
+// }
 
-class RegisteredPerson extends Person {
-  constructor(
-    public name: string,
-    public email: string,
-    public phone: number
-  ) {
-    super();
-  }
-}
+// /////////
 
-class RegisteredUser implements User {}
+// class RegisteredPerson extends Person {
+//   constructor(
+//     public name: string,
+//     public email: string,
+//     public phone: number
+//   ) {
+//     super();
+//   }
+// }
 
-/////////
+// interface Greeting {
+//   greeting: () => void;
+// }
 
-const person: RegisteredPerson = new RegisteredPerson (
-  "John",
-  "john@example.com",
-  123456789,
-);
+// class RegisteredUser implements User, Greeting {
+//   constructor(
+//     public name: string,
+//     public email: string,
+//     public phone: number
+//   ) {}
 
-console.log(person);
+//   public greeting () {
+//     console.log(`Hello ${this.name}`);
+//   };
+// }
+
+// /////////
+
+// const person: RegisteredPerson = new RegisteredPerson (
+//   "John",
+//   "john@example.com",
+//   123456789,
+// );
+
+// console.log(person);
+
+// console.log(Person.nameClass());
