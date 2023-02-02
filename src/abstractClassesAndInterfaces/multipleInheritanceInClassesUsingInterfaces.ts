0ed05c068a82c036793f11f3ca58503f7b45587a
sychova <anastasiya.sychova@gmail.com>
2023-02-02 18:02:34 +0300

    section 6 part 7 mult inheritance in classes using interfaces

---
+++ src/abstractClassesAndInterfaces/multipleInheritanceInClassesUsingInterfaces.ts
@@ -0,0 +1,9 @@
+// class User {
+//   constructor(public name: string) {}
+// }
+
+// class Password {
+//   constructor(public password: string) {}
+// }
+
+// class RegisteredUser extends User, Password {}
