8ed9fcf96dd528131d8d521a7f668a327415d3ab
sychova <anastasiya.sychova@gmail.com>
2023-05-30 15:37:35 +0300

    decorators

--- ts-compiler/dist/app.js
+++ ts-compiler/dist/app.js
@@ -8,51 +8,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var Manufacturers;
-(function (Manufacturers) {
-    Manufacturers["BOEING"] = "boeing";
-    Manufacturers["AIRBUS"] = "airbus";
-})(Manufacturers || (Manufacturers = {}));
-function AircraftManufacturer(manufacturer) {
-    return (target) => {
-        if (manufacturer === Manufacturers.AIRBUS) {
-            target.prototype.origin = "USA";
-            target.prototype.manufacturer = Manufacturers.AIRBUS;
-            target.prototype.type = "Jet";
-            target.prototype.airbusMethod = () => {
-                console.log("Function by Airbus");
-            };
-        }
-        else {
-            target.prototype.origin = "France";
-            target.prototype.manufacturer = Manufacturers.BOEING;
-            target.prototype.type = "Helicopter";
-            target.prototype.boingMethod = () => {
-                console.log("Function by Boing");
-            };
-        }
+function AddLocation(lat, long) {
+    return (classConstructor) => {
+        return class extends classConstructor {
+            constructor(...args) {
+                super(...args);
+                this.mapLocation = { lat, long };
+            }
+        };
     };
 }
-let Airplane = class Airplane {
-    constructor(_aircraftModel, pilot) {
-        this._aircraftModel = _aircraftModel;
-        this.pilot = pilot;
-        console.log("Class initiated");
-    }
-    pilotName() {
-        console.log(this.pilot);
-    }
-    get aircraftModel() {
-        return this._aircraftModel;
+let Person = class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
 };
-Airplane = __decorate([
-    AircraftManufacturer(Manufacturers.AIRBUS),
-    __metadata("design:paramtypes", [String, String])
-], Airplane);
-const airplane = new Airplane("Airbus A380", "John");
-console.log(airplane.manufacturer);
-airplane.airbusMethod
-    ? airplane.airbusMethod()
-    : console.log("Method does not exist");
+Person = __decorate([
+    AddLocation(1.234, 1.876),
+    __metadata("design:paramtypes", [String, Number])
+], Person);
+const person = new Person("John", 32);
+console.log(person);
 //# sourceMappingURL=app.js.map
--- ts-compiler/src/app.ts
+++ ts-compiler/src/app.ts
@@ -1,57 +1,24 @@
-enum Manufacturers {
-  BOEING = "boeing",
-  AIRBUS = "airbus",
-}
-
-interface AircraftInterface {
-  _aircraftModel: string;
-  prototype?: any;
-  origin?: string;
-  manufacturer?: string;
-  type?: string;
-  airbusMethod?: () => void;
-  boingMethod?: () => void;
-}
-
-function AircraftManufacturer(manufacturer: Manufacturers) {
-  return (target: Function) => {
-    if (manufacturer === Manufacturers.AIRBUS) {
-      target.prototype.origin = "USA";
-      target.prototype.manufacturer = Manufacturers.AIRBUS;
-      target.prototype.type = "Jet";
-      target.prototype.airbusMethod = () => {
-        console.log("Function by Airbus");
-      };
-    } else {
-      target.prototype.origin = "France";
-      target.prototype.manufacturer = Manufacturers.BOEING;
-      target.prototype.type = "Helicopter";
-      target.prototype.boingMethod = () => {
-        console.log("Function by Boing");
-      };
-    }
+function AddLocation(lat: number, long: number) {
+  return <T extends { new (...args: any[]): {} }>(classConstructor: T) => {
+    return class extends classConstructor {
+      mapLocation: MapLocation;
+      constructor(...args: any[]) {
+        super(...args);
+        this.mapLocation = { lat, long };
+      }
+    };
   };
 }
 
-@AircraftManufacturer(Manufacturers.AIRBUS)
-class Airplane implements AircraftInterface {
-  constructor(public _aircraftModel: string, private pilot: string) {
-    console.log("Class initiated");
-  }
-
-  public pilotName() {
-    console.log(this.pilot);
-  }
-
-  public get aircraftModel() {
-    return this._aircraftModel;
-  }
+interface MapLocation {
+  lat: number;
+  long: number;
 }
 
-const airplane: AircraftInterface = new Airplane("Airbus A380", "John");
+@AddLocation(1.234, 1.876)
+class Person {
+  constructor(public name: string, public age: number) {}
+}
 
-console.log(airplane.manufacturer);
-
-airplane.airbusMethod
-  ? airplane.airbusMethod()
-  : console.log("Method does not exist");
+const person: Person = new Person("John", 32);
+console.log(person);
