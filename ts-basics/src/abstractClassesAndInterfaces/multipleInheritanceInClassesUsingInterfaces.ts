b3d30a4312712cc8c177f000ce65aed60444dd74
sychova <anastasiya.sychova@gmail.com>
2023-11-08 18:17:30 +0300

    delete theory code

--- ts-basics/src/abstractClassesAndInterfaces/multipleInheritanceInClassesUsingInterfaces.ts
+++
@@ -1,9 +0,0 @@
-// class User {
-//   constructor(public name: string) {}
-// }
-
-// class Password {
-//   constructor(public password: string) {}
-// }
-
-// class RegisteredUser extends User, Password {}
--- ts-basics/src/types/unknown.type.ts
+++
@@ -1,6 +0,0 @@
-// const fooUnknown = (bar: unknown) => {
-//   if (typeof bar === "number") return bar * 2
-// }
-
-// console.log(fooUnknown(4))
-// console.log(fooUnknown("bar"))
